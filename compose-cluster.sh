d96e45247372004329ecb71b537d01fa0b06673b
jtmb <james.branco@gmail.com>
2023-07-22 02:44:56 -0400

    initial commit

---
+++ compose-cluster.sh
@@ -0,0 +1,17 @@
+# ----------VARS
+key_pair=$(cat ~/.ssh/id_rsa.pub)
+userid=$(whoami)
+public_ip=$(curl icanhazip.com)
+admin_pub_ip=$(echo ['"'$public_ip/32'"'])
+echo "userid=$userid" > docker-compose/jenkins/.env
+# ----------Instance IP's
+master_node=192.168.0.24
+worker_node_1=192.168.0.19
+# --------------------------------------------------------------------
+
+cd ansible_provisioning && ANSIBLE_CONFIG=./ansible.cfg ansible-playbook -i inventory.ini main.yml --ask-become-pass --ask-pass --limit 'all' --skip-tags "packages" --tags "jenkins" \
+    --extra-vars "ssh_port=2002 user_id=$userid \
+    master_node=$master_node  \
+    worker_node_1=$worker_node_1 \
+    worker_node_2=$worker_node_2  \  
+    ssh_cert=$HOME/.ssh/id_rsa"
