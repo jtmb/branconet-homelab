265296dcded72ce0b29710bf94ebf7333a16d1c5
jtmb <james.branco@gmail.com>
2023-08-03 22:34:24 -0400

    updated pi service

--- compose-cluster.sh
+++ compose-cluster.sh
@@ -4,7 +4,6 @@ userid=$(whoami)
 public_ip=$(curl icanhazip.com)
 admin_pub_ip=$(echo ['"'$public_ip/32'"'])
 echo "userid=$userid" > docker-compose/jenkins/.env
-sudo_pass=$(cat ~/secrets/sudo_pass)
 pub_key=$(cat ~/.ssh/id_rsa.pub)
 # ----------Instance IP's
 master_node=192.168.0.24
@@ -13,7 +12,7 @@ worker_node_2=192.168.0.19
 # --------------------------------------------------------------------
 
 cd ansible_provisioning && ANSIBLE_CONFIG=./ansible.cfg ansible-playbook -i inventory.ini main.yml \
-    --ask-become-pass --ask-pass --limit 'all' --skip-tags "jenkins" --tags "swarm" \
+    --ask-become-pass --ask-pass --limit 'all' --skip-tags "jenkins" --tags "containers" \
     --extra-vars "ssh_port=2002 pub_key=$pub_key \
     master_node=$master_node  \
     worker_node_1=$worker_node_1 \
