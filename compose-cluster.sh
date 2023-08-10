c66d3bc15524e878008bb3e664e55991b2e7abd1
jtmb <james.branco@gmail.com>
2023-08-10 12:49:10 -0400

    SSH AUTH & PLUGINS

--- compose-cluster.sh
+++ compose-cluster.sh
@@ -2,7 +2,6 @@
 userid=$(whoami)
 public_ip=$(curl icanhazip.com)
 admin_pub_ip=$(echo ['"'$public_ip/32'"'])
-pub_key=$(cat ~/.ssh/id_rsa.pub)
 # ----------Instance IP's
 master_node=192.168.0.24
 worker_node_1=192.168.0.16
@@ -10,11 +9,11 @@ worker_node_2=192.168.0.19
 # --------------------------------------------------------------------
 
 cd ansible_provisioning && ANSIBLE_CONFIG=./ansible.cfg ansible-playbook -i inventory.ini main.yml \
-    --ask-become-pass --ask-pass --limit 'all' --skip-tags "reboot" --tags "mc" \
+    --ask-become-pass --ask-pass --limit 'master_node' --skip-tags "reboot" --tags "ssh" \
     --extra-vars "ssh_port=2002 pub_key=$pub_key \
     master_node=$master_node  \
     worker_node_1=$worker_node_1 \
     worker_node_2=$worker_node_2  \  
     ssh_cert=$HOME/.ssh/id_rsa \
-    user_id=$userid pub_key=$pub_key \
+    user_id=$userid  \
     PUB_IP=$public_ip"
