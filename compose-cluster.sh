a7d5525b453a03f4222ef6563f31c7ac9d675833
jtmb <james.branco@gmail.com>
2023-08-15 13:47:06 -0400

    updates

--- compose-cluster.sh
+++ compose-cluster.sh
@@ -2,7 +2,7 @@
 userid=$(whoami)
 public_ip=$(curl icanhazip.com)
 admin_pub_ip=$(echo ['"'$public_ip/32'"'])
-homedir=/home/$user_id/repos/media-stack
+homedir=/home/$userid/repos/media-stack
 # ----------Instance IP's
 master_node=192.168.0.24
 worker_node_1=192.168.0.16
@@ -10,12 +10,12 @@ worker_node_2=192.168.0.19
 # --------------------------------------------------------------------
 
 cd ansible_provisioning && ANSIBLE_CONFIG=./ansible.cfg ansible-playbook -i inventory.ini main.yml \
-    --limit 'master_node' --skip-tags "reboot" --tags "mc" \
+    --limit 'master_node' --skip-tags "reboot" --tags "backup_mc" \
     --extra-vars "ssh_port=2002 pub_key=$pub_key \
     master_node=$master_node  \
     worker_node_1=$worker_node_1 \
     worker_node_2=$worker_node_2  \  
-    ssh_cert=$HOME/.ssh/id_rsa \
+    ssh_cert=$HOME/.ssh/id_rsa homedir=$homedir \
     user_id=$userid  \
     PUB_IP=$public_ip \
     ansible_sudo_pass="
