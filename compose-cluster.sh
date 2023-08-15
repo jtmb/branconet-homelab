6751a8289a0fe306f4a87c20ef8fe2be10318c4c
jtmb <james.branco@gmail.com>
2023-08-15 10:30:36 -0400

    updated with jenkins job compat

--- compose-cluster.sh
+++ compose-cluster.sh
@@ -2,6 +2,7 @@
 userid=$(whoami)
 public_ip=$(curl icanhazip.com)
 admin_pub_ip=$(echo ['"'$public_ip/32'"'])
+homedir=/home/{{user_id}}/repos/
 # ----------Instance IP's
 master_node=192.168.0.24
 worker_node_1=192.168.0.16
@@ -9,11 +10,12 @@ worker_node_2=192.168.0.19
 # --------------------------------------------------------------------
 
 cd ansible_provisioning && ANSIBLE_CONFIG=./ansible.cfg ansible-playbook -i inventory.ini main.yml \
-    --ask-become-pass --ask-pass --limit 'master_node' --skip-tags "reboot" --tags "mc,containers" \
+    --limit 'master_node' --skip-tags "reboot" --tags "mc" \
     --extra-vars "ssh_port=2002 pub_key=$pub_key \
     master_node=$master_node  \
     worker_node_1=$worker_node_1 \
     worker_node_2=$worker_node_2  \  
     ssh_cert=$HOME/.ssh/id_rsa \
     user_id=$userid  \
-    PUB_IP=$public_ip"
+    PUB_IP=$public_ip \
+    ansible_sudo_pass="
