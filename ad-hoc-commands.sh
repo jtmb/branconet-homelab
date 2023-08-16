0789db68751ee9be045eb882b63ae0e935814270
jtmb <james.branco@gmail.com>
2023-08-16 13:56:02 -0400

    updated to work with wrapper script

--- ad-hoc-commands.sh
+++ ad-hoc-commands.sh
@@ -1,21 +1,17 @@
-# -----------------Vault Auth
-AUTH_TOKEN=$(cat /$HOME/.secrets/vault_token)
-VAULT_SERVER=$(cat /$HOME/.secrets/vault_server)
-export VAULT_ADDR=$VAULT_SERVER
-export VAULT_TOKEN=$AUTH_TOKEN
-vault login $VAULT_TOKEN
-ANSIBLE_SUDO_PASS=$(vault kv get -field=admin_pwd kv/admin_pass)
 # ----------VARS
 userid=$(whoami)
+homedir=/home/$userid/repos/media-stack
 public_ip=$(curl icanhazip.com)
 admin_pub_ip=$(echo ['"'$public_ip/32'"'])
-homedir=/home/$userid/repos/media-stack
-# ----------Instance IP's
+# -----------------Vault Auth
+source $homedir/ansible_provisioning/wrapper-scripts/vault-auth.sh
+# --------------------------Vault VARS
+ANSIBLE_SUDO_PASS=$(vault kv get -field=admin_pwd kv/admin_pass)
+# ---------Instance IP's
 master_node=192.168.0.24
 worker_node_1=192.168.0.16
 worker_node_2=192.168.0.19
 # --------------------------------------------------------------------
-
 cd ansible_provisioning && ansible all -m ansible.builtin.shell -a 'rm -rfv /home/ubuntu/container-program-files/plex_smb_share/' \
     --extra-vars "ssh_port=2002 \
     master_node=$master_node  \
