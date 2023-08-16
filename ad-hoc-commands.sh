e2a16fa0df842c7ff615a993e3d8732da7406d37
jtmb <james.branco@gmail.com>
2023-08-16 00:15:23 -0400

    added reboot commands

--- ad-hoc-commands.sh
+++ ad-hoc-commands.sh
@@ -1,7 +1,5 @@
 # ----------VARS
 userid=$(whoami)
-public_ip=$(curl icanhazip.com)
-admin_pub_ip=$(echo ['"'$public_ip/32'"'])
 homedir=/home/$userid/repos/media-stack
 # ----------Instance IP's
 master_node=192.168.0.24
@@ -9,13 +7,15 @@ worker_node_1=192.168.0.16
 worker_node_2=192.168.0.19
 # --------------------------------------------------------------------
 
-cd ansible_provisioning && ANSIBLE_CONFIG=./ansible.cfg ansible-playbook -i inventory.ini main.yml \
-    --limit 'all' --skip-tags "reboot" --tags "containers" \
-    --extra-vars "ssh_port=2002 pub_key=$pub_key \
+cd ansible_provisioning && ansible master_node -m ansible.builtin.shell -a 'echo test' \
+    --extra-vars "ssh_port=2002 \
     master_node=$master_node  \
     worker_node_1=$worker_node_1 \
     worker_node_2=$worker_node_2  \  
-    ssh_cert=$HOME/.ssh/id_rsa homedir=$homedir \
+    homedir=$homedir \
     user_id=$userid  \
     PUB_IP=$public_ip \
+    ssh_user=james \
     ansible_sudo_pass="
+
+
