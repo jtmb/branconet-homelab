14ce6db3e8ce64a96475875e6755b7c9bbc732e7
jtmb <james.branco@gmail.com>
2023-08-16 12:40:15 -0400

    updated with vault

--- ad-hoc-commands.sh
+++ ad-hoc-commands.sh
@@ -1,5 +1,14 @@
+# -----------------Vault Auth
+AUTH_TOKEN=$(cat /$HOME/.secrets/vault_token)
+VAULT_SERVER=$(cat /$HOME/.secrets/vault_server)
+export VAULT_ADDR=$VAULT_SERVER
+export VAULT_TOKEN=$AUTH_TOKEN
+vault login $VAULT_TOKEN
+ANSIBLE_SUDO_PASS=$(vault kv get -field=admin_pwd kv/admin_pass)
 # ----------VARS
 userid=$(whoami)
+public_ip=$(curl icanhazip.com)
+admin_pub_ip=$(echo ['"'$public_ip/32'"'])
 homedir=/home/$userid/repos/media-stack
 # ----------Instance IP's
 master_node=192.168.0.24
@@ -7,15 +16,15 @@ worker_node_1=192.168.0.16
 worker_node_2=192.168.0.19
 # --------------------------------------------------------------------
 
-cd ansible_provisioning && ansible master_node -m ansible.builtin.shell -a 'echo test' \
+cd ansible_provisioning && ansible all -m ansible.builtin.shell -a 'rm -rfv /home/ubuntu/container-program-files/plex_smb_share/' \
     --extra-vars "ssh_port=2002 \
     master_node=$master_node  \
     worker_node_1=$worker_node_1 \
     worker_node_2=$worker_node_2  \  
     homedir=$homedir \
     user_id=$userid  \
-    PUB_IP=$public_ip \
-    ssh_user=james \
-    ansible_sudo_pass="
+    ssh_cert=$HOME/.ssh/id_rsa PUB_IP=$public_ip \
+    ssh_user=james ansible_become=true \
+    ansible_sudo_pass=$ANSIBLE_SUDO_PASS"
 
 
--- compose-cluster.sh
+++ compose-cluster.sh
@@ -1,3 +1,10 @@
+# -----------------Vault Auth
+AUTH_TOKEN=$(cat /$HOME/.secrets/vault_token)
+VAULT_SERVER=$(cat /$HOME/.secrets/vault_server)
+export VAULT_ADDR=$VAULT_SERVER
+export VAULT_TOKEN=$AUTH_TOKEN
+vault login $VAULT_TOKEN
+ANSIBLE_SUDO_PASS=$(vault kv get -field=admin_pwd kv/admin_pass)
 # ----------VARS
 userid=$(whoami)
 public_ip=$(curl icanhazip.com)
@@ -10,7 +17,7 @@ worker_node_2=192.168.0.19
 # --------------------------------------------------------------------
 
 cd ansible_provisioning && ANSIBLE_CONFIG=./ansible.cfg ansible-playbook -i inventory.ini main.yml \
-    --limit 'master_node' --skip-tags "reboot" --tags "containers" \
+    --limit 'all' --skip-tags "reboot" --tags "containers" \
     --extra-vars "ssh_port=2002 pub_key=$pub_key \
     master_node=$master_node  \
     worker_node_1=$worker_node_1 \
@@ -18,4 +25,18 @@ cd ansible_provisioning && ANSIBLE_CONFIG=./ansible.cfg ansible-playbook -i inve
     ssh_cert=$HOME/.ssh/id_rsa homedir=$homedir \
     user_id=$userid  \
     PUB_IP=$public_ip \
-    ansible_sudo_pass="
+    ansible_sudo_pass=$ANSIBLE_SUDO_PASS"
+
+
+# TAGS 
+    # containers
+    # swarm 
+    # update_ubuntu
+    # pre_prov
+    # backup_all
+    # backup_mc
+    # smb 
+    # reboot 
+    # dns 
+    # mc 
+    # ssh 
