014d09e3107f3ccf8808ee1c5149f71b4103206b
jtmb <james.branco@gmail.com>
2023-08-18 19:34:29 -0400

    updated adhoc

--- ad-hoc-commands.sh
+++ ad-hoc-commands.sh
@@ -1,27 +1,30 @@
-# ----------VARS
-userid=$(whoami)
-homedir=/home/$userid/repos/media-stack
-public_ip=$(curl icanhazip.com)
-admin_pub_ip=$(echo ['"'$public_ip/32'"'])
-# -----------------Vault Auth
-source $homedir/ansible_provisioning/wrapper-scripts/vault-auth.sh
-# --------------------------Vault VARS
-ANSIBLE_SUDO_PASS=$(vault kv get -field=admin_pwd kv/admin_pass)
-# ---------Instance IP's
-master_node=$(vault kv get -field=ip kv/master_node)
-worker_node_1=$(vault kv get -field=ip kv/worker_node_1)
-worker_node_2=$(vault kv get -field=ip kv/worker_node_2)
-# --------------------------------------------------------------------
+# VARS
+source ansible_provisioning/vars/vars.sh
 
+# Ansible Playbook Consumes VARS
 cd ansible_provisioning && ansible all -m ansible.builtin.shell -a 'rm -rfv /home/ubuntu/container-program-files/plex_smb_share/' \
-    --extra-vars "ssh_port=2002 \
+    --extra-vars "ssh_port=$SSH_PORT pub_key=$pub_key \
     master_node=$master_node  \
     worker_node_1=$worker_node_1 \
     worker_node_2=$worker_node_2  \  
-    homedir=$homedir \
+    ssh_cert=$HOME/.ssh/id_rsa homedir=$homedir \
+    container_volumes_location=$container_volumes_location \
     user_id=$userid  \
-    ssh_cert=$HOME/.ssh/id_rsa PUB_IP=$public_ip \
-    ssh_user=james ansible_become=true \
-    ansible_sudo_pass=$ANSIBLE_SUDO_PASS"
+    PUB_IP=$public_ip cf_key=$cf_key cf_zone_id=$cf_zone_id domain_name=$domain_name \
+    ssh_user=$ANSIBLE_SSH_USER ansible_sudo_pass=$ANSIBLE_SUDO_PASS"
+
+# TAGS 
+    # containers
+    # swarm 
+    # update_ubuntu
+    # pre_prov
+    # backup_all
+    # backup_mc
+    # smb 
+    # reboot 
+    # dns 
+    # mc 
+    # ssh 
+    # cloudflare
 
 
