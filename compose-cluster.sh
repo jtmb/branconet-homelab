e0f4d5ea9673e75b1f810dc300b0a761d7f05227
jtmb <james.branco@gmail.com>
2023-11-22 21:34:04 -0500

    updated plex

--- compose-cluster.sh
+++ compose-cluster.sh
@@ -4,16 +4,17 @@ source ansible_provisioning/vars/vars.sh
 # Ansible Playbook Consumes VARS
 cd ansible_provisioning && \
      ANSIBLE_CONFIG=./ansible.cfg ansible-playbook -i inventory.ini main.yml \
-    --limit 'master_node' --skip-tags "reboot" --tags "images" \
+    --limit 'all' --skip-tags "reboot,cicd_containers" --tags "swarm,containers,smb" \
     --extra-vars "ssh_port=$SSH_PORT pub_key=$pub_key \
     master_node=$master_node  \
-    worker_node_1=$worker_node_1 \
+    # worker_node_1=$worker_node_1 \
     worker_node_2=$worker_node_2  \  
     ssh_cert=$HOME/.ssh/id_rsa homedir=$homedir \
     container_volumes_location=$container_volumes_location \
     user_id=$userid  \
-    discord_webook=$discord_webook PUB_IP=$public_ip cf_key=$cf_key cf_zone_id=$cf_zone_id domain_name=$domain_name domain_name=$domain_name email=$email \
-    ssh_user=$ANSIBLE_SSH_USER ansible_sudo_pass=$ANSIBLE_SUDO_PASS"
+    discord_webhook=$discord_webhook PUB_IP=$public_ip cf_key=$cf_key cf_zone_id=$cf_zone_id domain_name=$domain_name email=$email \
+    nord_user=$nord_user nord_pass=$nord_pass \
+    ssh_user=$ANSIBLE_SSH_USER ansible_sudo_pass=$ANSIBLE_SUDO_PASS "
 
 # TAGS 
     # containers
