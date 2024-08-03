91c8f4d9b54ceeb17941291fc040753f3eaae1b5
jtmb <james.branco@gmail.com>
2024-08-03 19:01:48 -0400

    updated

--- ansible_provisioning/vars/vars.sh
+++ ansible_provisioning/vars/vars.sh
@@ -1,4 +1,3 @@
-
 # ###########################################################################################################################################################
 # #                                                              !!!SET VARS HERE!!!                                                                      # #
 # #   Vars must be set in order for ansible deploy to work. If you are unsure about what these VARS do, search for them in the project and get familiar.  # #
@@ -15,23 +14,21 @@ admin_pub_ip=$(echo ['"'$public_ip/32'"'])
 source $homedir/ansible_provisioning/wrapper-scripts/vault-auth.sh 
 
 # ANSIBLE VARS
-# ANSIBLE_SUDO_PASS=$(vault kv get -field=admin_pwd kv/admin_pass)
-ANSIBLE_SUDO_PASS=rancherprd1234!
-# ANSIBLE_SSH_USER=$(vault kv get -field=admin_usr kv/admin_user)
-ANSIBLE_SSH_USER=rancher
-SSH_PORT=22 #$(vault kv get -field=port kv/ssh_port)
-# container_volumes_location=$(vault kv get -field=value kv/container_volumes_location)
-container_volumes_location=/mnt/container-program-files
+ANSIBLE_SUDO_PASS=$(vault kv get -field=admin_pwd kv/admin_pass)
+ANSIBLE_SSH_USER=$(vault kv get -field=admin_usr kv/admin_user)
+SSH_PORT=$(vault kv get -field=port kv/ssh_port)
+container_volumes_location=$(vault kv get -field=value kv/container_volumes_location)
 
 # INSTANCE IP's
-master_node=192.168.0.25
-worker_node_1=192.168.0.26
-worker_node_2=192.168.0.27
+master_node=192.168.0.5
+# worker_node_1=192.168.0.4
+# worker_node_2=192.168.0.7
 
-# CLOUDFLARE
+# DNS
 cf_key=$(vault kv get -field=key kv/cf_key)
 cf_zone_id=$(vault kv get -field=id kv/cf_zone_id)
-domain_name=test.lan #$(vault kv get -field=value kv/domain_name)
+domain_name=$(vault kv get -field=value kv/domain_name)
+lan_domain_name=$(vault kv get -field=value kv/lan_domain_name)
 discord_webhook=$(vault kv get -field=value kv/discord_webhook)
 email=$(vault kv get -field=value kv/email)
 
--- compose-cluster.sh
+++ compose-cluster.sh
@@ -4,7 +4,7 @@ source ansible_provisioning/vars/vars.sh
 # Ansible Playbook Consumes VARS
 cd ansible_provisioning && \
      ANSIBLE_CONFIG=./ansible_provisioning/ansible.cfg ansible-playbook -i inventory.ini main.yml \
-    --limit 'all' --skip-tags "reboot,images,cloudflare" --tags "dns" \
+    --limit 'all' --skip-tags "reboot,images,cloudflare" --tags "swarm,containers" \
     --extra-vars "ssh_port=$SSH_PORT pub_key=$pub_key \
     master_node=$master_node  \
     worker_node_1=$worker_node_1 \
@@ -12,7 +12,7 @@ cd ansible_provisioning && \
     ssh_cert=$HOME/.ssh/id_ed25519 homedir=$homedir \
     container_volumes_location=$container_volumes_location \
     user_id=$userid  \
-    discord_webhook=$discord_webhook PUB_IP=$public_ip cf_key=$cf_key cf_zone_id=$cf_zone_id domain_name=$domain_name email=$email \
+    discord_webhook=$discord_webhook PUB_IP=$public_ip cf_key=$cf_key cf_zone_id=$cf_zone_id domain_name=$domain_name lan_domain_name=$lan_domain_name email=$email \
     nord_user=$nord_user nord_pass=$nord_pass \
     ssh_user=$ANSIBLE_SSH_USER ansible_sudo_pass=$ANSIBLE_SUDO_PASS "
 
