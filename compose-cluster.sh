ad64e286524514b2b5d858efd9fbbe084a6ebf32
username-here <your-email-here>
2024-08-14 21:13:04 -0400

    updated homepage settings

--- compose-cluster.sh
+++ compose-cluster.sh
@@ -4,7 +4,7 @@ source ansible_provisioning/vars/vars.sh
 # Ansible Playbook Consumes VARS
 cd ansible_provisioning && \
      ANSIBLE_CONFIG=./ansible_provisioning/ansible.cfg ansible-playbook -i inventory.ini main.yml \
-    --limit 'all' --skip-tags "reboot,images,cloudflare" --tags "fail2ban" \
+    --limit 'all' --skip-tags "reboot,images,cloudflare" --tags "plex" \
     --extra-vars "ssh_port=$SSH_PORT pub_key=$pub_key \
     master_node=$master_node  \
     worker_node_1=$worker_node_1 \
@@ -17,6 +17,7 @@ cd ansible_provisioning && \
     ssh_user=$ANSIBLE_SSH_USER ansible_sudo_pass=$ANSIBLE_SUDO_PASS "
 
 # TAGS 
+    # fail2ban
     # containers
     # swarm 
     # update_ubuntu
