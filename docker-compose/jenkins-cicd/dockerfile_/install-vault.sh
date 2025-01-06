92a5cc990e7f6e974868cb148880b0cab11ceff7
James <86915618+jtmb@users.noreply.github.com>
2025-01-06 06:37:59 -0500

    Glusterfs (#8)
    * working model
    
    * added glustefs, round robbin DNS + traefik
    
    * updated
    
    * updated gluster permissions
    
    * added glusterfs multi node support
    
    * updated
    
    * added fail2ban role
    
    * updated homepage settings
    
    * updated vars
    
    * updated readme
    
    * updated
    
    * updated node exporter to identify node hosts
    
    * locks.mandatory-locking optimal for glusterfs
    
    * trimmed the fat
    
    * updated playbook with wp
    
    * updated playbook
    
    * added config
    
    * updated
    
    * removed
    
    ---------
    
    Co-authored-by: username-here <your-email-here>

--- docker-compose/jenkins-cicd/dockerfile_/install-vault.sh
+++
@@ -1,8 +0,0 @@
-# Download the signing key to a new keyring
-wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
-# Add the HashiCorp repo
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | tee /etc/apt/sources.list.d/hashicorp.list
-# Update Repositorys
-apt update
-# Install Vault
-apt install vault -y
