97ed74f397bc2f05a5f752675b6c17d76124b64a
jtmb <james.branco@gmail.com>
2025-01-06 05:54:32 -0500

    updated

--- docker-compose/swarm/torrent-stack/port-forewarder.sh
+++ docker-compose/swarm/torrent-stack/port-forewarder.sh
@@ -5,6 +5,6 @@ cat /tmp/gluetun/forwarded_port
 tcpport=$(cat /tmp/gluetun/forwarded_port)
 echo "tcpport is set to $tcpport"
 echo "Setting listen_port to $tcpport"
-curl --cookie-jar /tmp/cookie.txt -i --header 'Referer: http://192.168.0.5:8112' --data 'username=brajam&password=RunStr8@1!' http://192.168.0.5:8112/api/v2/auth/login
+curl --cookie-jar /tmp/cookie.txt -i --header 'Referer: http://192.168.0.5:8112' --data 'username=admin&password=admin!' http://192.168.0.5:8112/api/v2/auth/login
 curl -s -b /tmp/cookie.txt "http://192.168.0.5:8112/api/v2/app/setPreferences" -d 'json={"listen_port": "'"$tcpport"'"}'
 while :; do sleep 1; done
