527ae8c4ad1fbcf579325ea85e5a6495bcd4f92b
James <86915618+jtmb@users.noreply.github.com>
2025-01-06 06:37:59 -0500

    Glusterfs (#8)
    * working model
    
    * added glustefs, round robbin DNS + traefik
    
    * updated
    
    * updated gluster permissions
    
    * added glusterfs multi node support
    
    * updated
    
    * added fail2ban role
    
    * updated homepage settings
    
    * updated vars
    
    * updated readme
    
    * updated
    
    * updated node exporter to identify node hosts
    
    * locks.mandatory-locking optimal for glusterfs
    
    * trimmed the fat
    
    * updated playbook with wp
    
    * updated playbook
    
    * added config
    
    * updated
    
    * removed
    
    ---------
    
    Co-authored-by: username-here <your-email-here>

--- README.md
+++ README.md
@@ -1,4 +1,22 @@
-# <h1> media-stack
+# <h1> branconet-homelab
+
+My personal highly avilable, customizable homelab as code deployment & pipeline which features:
+
+- Ansible
+- Docker swarm
+- Traefik
+- Rancher
+- GlusterFS
+- DNS roundrobbin setup
+- Hashicorp Vault
+- Fail2Ban
+- ZFS
+- Backups as service
+- Highly available monitoring stack
+- Highly available media stack
+- Highly available webhosting
+- Custom image build pipeline
+
 
 ##### ANSIBLE_VERSION="2.16"
 ##### SUPPORTED_OS_VERSIONS="UBUNTU_LTS_20.04 => 24.04"
--- ansible_provisioning/vars/vars.sh
+++ ansible_provisioning/vars/vars.sh
@@ -1,4 +1,3 @@
-
 # ###########################################################################################################################################################
 # #                                                              !!!SET VARS HERE!!!                                                                      # #
 # #   Vars must be set in order for ansible deploy to work. If you are unsure about what these VARS do, search for them in the project and get familiar.  # #
@@ -7,7 +6,7 @@
 
 # GENERAL VARS
 userid=$(whoami)
-homedir=/home/$userid/repos/media-stack
+homedir=/home/$userid/repos/branconet-homelab
 public_ip=$(curl icanhazip.com)
 admin_pub_ip=$(echo ['"'$public_ip/32'"'])
 
@@ -19,19 +18,31 @@ ANSIBLE_SUDO_PASS=$(vault kv get -field=admin_pwd kv/admin_pass)
 ANSIBLE_SSH_USER=$(vault kv get -field=admin_usr kv/admin_user)
 SSH_PORT=$(vault kv get -field=port kv/ssh_port)
 container_volumes_location=$(vault kv get -field=value kv/container_volumes_location)
+nfs_volumes_location=/mnt/nfs-container-volumes
+database_password=$(vault kv get -field=value kv/database_password)
+
 
 # INSTANCE IP's
 master_node=192.168.0.5
-worker_node_1=192.168.0.4
-worker_node_2=192.168.0.7
+worker_node_1=192.168.0.9
+worker_node_2=192.168.0.8
+mini_linux=192.168.0.4
+truenas=192.168.0.3
 
-# CLOUDFLARE
+# DNS
 cf_key=$(vault kv get -field=key kv/cf_key)
 cf_zone_id=$(vault kv get -field=id kv/cf_zone_id)
 domain_name=$(vault kv get -field=value kv/domain_name)
+lan_domain_name=$(vault kv get -field=value kv/lan_domain_name)
 discord_webhook=$(vault kv get -field=value kv/discord_webhook)
 email=$(vault kv get -field=value kv/email)
 
 # VPN
 nord_user=$(vault kv get -field=value kv/nord_user)
 nord_pass=$(vault kv get -field=value kv/nord_password)
+protonvpn_user=$(vault kv get -field=value kv/protonvpn_user)
+protonvpn_pass=$(vault kv get -field=value kv/protonvpn_pass)
+
+
+# BOTS
+RUCKUS_BOT_TOKEN=$(vault kv get -field=value kv/ruckus_bot_token)
